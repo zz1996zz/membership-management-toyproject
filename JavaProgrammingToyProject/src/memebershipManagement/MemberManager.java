95ddc6b94481e0eafe9618117e989fa49f3af6b0
zz1996zz <zz1996zz@naver.com>
2022-05-03 15:20:02 +0900

    feat: 유효성 검증을 추가
    CRUD를 진행 시 입력값이 유효한지 검증하는 코드를 추가하였습니다.

--- JavaProgrammingToyProject/src/memebershipManagement/MemberManager.java
+++ JavaProgrammingToyProject/src/memebershipManagement/MemberManager.java
@@ -33,35 +33,68 @@ public class MemberManager {
 			}
 			case 2: {
 				Member member = new Member();
+				String msg;
 
-				System.out.print("아이디를 입력하세요 (형식 M-00001) : ");
-				member.setMember_id(br.readLine());
+				System.out.print("아이디를 입력하세요(형식 M-00001) : ");
+				msg = br.readLine();
+				if (validId(msg)) {
+					member.setMember_id(msg);
+				} else {
+					break;
+				}
 				System.out.print("이름을 입력하세요 : ");
-				member.setName(br.readLine());
+				msg = br.readLine();
+				if (validName(msg)) {
+					member.setName(msg);
+				} else {
+					break;
+				}
 				System.out.print("전화번호를 입력하세요 : ");
-				member.setPhone_number(br.readLine());
+				msg = br.readLine();
+				if (validPhoneNuber(msg)) {
+					member.setPhone_number(msg);
+				} else {
+					break;
+				}
 				memberDAO.insertMember(member);
 				System.out.println("---> 회원가입에 성공하셨습니다.");
 				break;
 			}
 			case 3: {
 				Member member = new Member();
-				
-				System.out.print("수정할 아이디를 입력하세요 (형식 M-00001) : ");
-				member.setMember_id(br.readLine());
+				String msg;
+
+				System.out.print("수정할 아이디를 입력하세요(형식 M-00001) : ");
+				msg = br.readLine();
+				if(validUpdateId(msg)) {
+					member.setMember_id(msg);					
+				} else {
+					break;
+				}
 				System.out.print("수정할 전화번호를 입력하세요 : ");
-				member.setPhone_number(br.readLine());
+				msg = br.readLine();
+				if(validPhoneNuber(msg)) {
+					member.setPhone_number(msg);					
+				} else {
+					break;
+				}
 				memberDAO.updateMember(member);
 				System.out.println("---> 회원수정에 성공하셨습니다.");
 				break;
 			}
 			case 4: {
 				Member member = new Member();
+				String msg;
 				
-				System.out.print("삭제할 아이디를 입력하세요 (형식 M-00001) : ");
-				member.setMember_id(br.readLine());
+				System.out.print("삭제할 아이디를 입력하세요(형식 M-00001) : ");
+				msg = br.readLine();
+				if(validDeleteId(msg)) {
+					member.setMember_id(msg);					
+				} else {
+					break;
+				}
 				memberDAO.deleteMember(member);
-				System.out.println("---> "+ member.getMember_id() + "회원 삭제에 성공하셨습니다.");
+				System.out.println("---> " + member.getMember_id() + "회원 삭제에 성공하셨습니다.");
 				break;
 			}
 			case 0: {
@@ -83,4 +116,65 @@ public class MemberManager {
 		System.out.println("종료를 원하시면 0번을 입력하세요.");
 	}
 
+	public boolean validId(String id) {
+
+		Member member = memberDAO.findMember(id);
+		if (id.isBlank()) {
+			System.out.println("아이디는 필수 입력항목입니다.");
+			return false;
+		} else if (id.charAt(0) != 'M' || id.length() != 7) {
+			System.out.println("아이디는 'M-'로 시작해야 하며, M-를 포함하여 7개의 문자로 구성해야 합니다.");
+			return false;
+		} else if (member.getMember_id() == "true") {
+			return true;
+		} else {
+			System.out.println(id + "가 이미 존재합니다.");
+			return false;
+		}
+	}
+
+	public boolean validName(String name) {
+
+		if (name.isBlank()) {
+			System.out.println("이름은 필수 입력항목입니다.");
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public boolean validPhoneNuber(String phoneNumber) {
+
+		if (phoneNumber.isBlank()) {
+			System.out.println("전화전호는 필수 입력항목입니다.");
+			return false;
+		} else if (phoneNumber.length() != 13 || (phoneNumber.charAt(3) != '-' && phoneNumber.charAt(8) != '-')) {
+			System.out.println("전화번호는 두 개의 '-'를 포함하여 총 13개의 문자로 구성해야 합니다.");
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public boolean validUpdateId(String id) {
+		
+		Member member = memberDAO.findMember(id);
+		if(member.getMember_id() == "true") {
+			System.out.println("수정할 " + id + "회원 정보가 존재하지 않습니다.");
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public boolean validDeleteId(String id) {
+
+		Member member = memberDAO.findMember(id);
+		if(member.getMember_id() == "true") {
+			System.out.println("삭제할 " + id + "회원 정보가 존재하지 않습니다.");
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
